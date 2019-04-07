20c846df7f0e808aa577faa8ae5c8846cecc85fb
Vittal KAMKAR <vittal.kamkar@amadeus.com>
2019-04-07 22:29:15 +0530

    Breadcrum Implemented

---
+++ REACTJS/BreadCrumb/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Layout from './containers/Layout/Layout'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Layout />
+    );
+  }
+}
+
+export default App;
